ab2efc623cb0ad4312acf5647cc5fb4dc7a5629c
Brandon <thebrandi451@gmail.com>
2022-08-14 20:02:40 +0200

    Added comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,26 @@
+//Imported the following hooks and components for use below
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 import RegisterOrLogin from './components/loginAndRegistration/RegisterOrLogin';
 import Repos from './components/repositories/Repos';
 import Users from './components/users/Users';
+
+//Functional component
 const App = () => {
+
+  //Returns the following
   return (
     <div className="App">
+
+      {/* Used browser router for routing */}
       <BrowserRouter>
         <Routes>
+          
+          {/* The following two routes displays the same component but with different props */} 
           <Route exact path="/register" element={<RegisterOrLogin/>}/>
           <Route exact path="/login" element={<RegisterOrLogin/>}/>
+
+          {/* Each route displays a different component  */}
           <Route exact path="/" element={<Home/>}/>
           <Route exact path="/repos" element={<Repos/>}/>
           <Route exact path="/users" element={<Users />}/>
@@ -19,4 +30,5 @@ const App = () => {
   );
 }
 
+//Exports the functional component
 export default App;
